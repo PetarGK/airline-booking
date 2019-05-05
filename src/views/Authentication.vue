45611b238179d10a62d1932ca23d28018a580380
Petar Korudzhiev <petar.korudzhiev@gmail.com>
2019-05-05 12:13:53 +0300

    Ep1 - add authentication to the project

--- src/views/Authentication.vue
+++ src/views/Authentication.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="row">
-    <amplify-authenticator class="authenticator__form"></amplify-authenticator>
+    <amplify-authenticator class="authenticator__form" v-bind:authConfig="authConfig"></amplify-authenticator>
   </div>
 </template>
 
@@ -30,7 +30,30 @@ export default {
         this.$router.push({ name: this.redirectTo });
       }
     });
-  }
+  },
+  data() {
+    return {
+      authConfig: {
+        signUpConfig: {
+          defaultCountryCode: "44",
+          signUpFields: [
+            {
+              label: "First name",
+              key: "given_name",
+              required: true,
+              displayOrder: 0
+            },
+            {
+              label: "Family name",
+              key: "family_name",
+              required: true,
+              displayOrder: 1
+            }
+          ]
+        }
+      }
+    }
+  }  
 };
 </script>
 
